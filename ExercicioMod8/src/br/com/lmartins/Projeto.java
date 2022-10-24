d374577a65574fac5b9d24241e7e6dabaebf80be
LMartinsgit <lucas.tbmartins@gmail.cmo>
2022-10-24 16:47:29 -0300

    first commit

--- ExercicioMod8/src/br/com/lmartins/Projeto.java
+++ ExercicioMod8/src/br/com/lmartins/Projeto.java
@@ -11,56 +11,69 @@ public class Projeto {
 			Scanner ler = new Scanner(System.in);
 			
 				String nome, materia1;
-				int nota1 = 0, nota2 = 0, nota3 = 0, nota4 = 0, media;
+				float nota1 = 0, nota2 = 0, nota3 = 0, nota4 = 0, media, mediapass;
+				
 				//media = ((double)(nota1 + nota2 + nota3 + nota4) / 4);
 				
 				
 				System.out.println("Olá, eu vou te ajudar a tirar sua média!");
 				Thread.sleep(1000);				
-				System.out.println("Me diga seu nome.");
+				System.out.println("Me diga seu nome:");
 				nome = ler.nextLine();
+				System.out.printf("%s, qual média você precisa para ser aprovado?\nUse ',' ao invés de '.': \n", nome);
+				mediapass = ler.nextFloat();
+				
 				
 				Thread.sleep(1000);
-				System.out.printf("\nVamos lá %s, vou tirar sua média!",nome);
+				System.out.printf("\nVamos lá %s, vou te ajudar!",nome);
 				Thread.sleep(1000);
-				System.out.printf("\nPreciso de 4 notas para tirar sua média.");
+				System.out.println("\nPreciso de 4 notas para tirar sua média.");
 				Thread.sleep(500);
-				System.out.printf("\nMe fale qual matéria e nota você deseja tirar sua média, como no exemplo abaixo:");
+				System.out.println("Me fale qual matéria e nota você deseja tirar sua média, como no exemplo abaixo:");
 				Thread.sleep(500);
-				System.out.printf("\n1ª Semestre: Português\nNota: 8,5\n");
+				System.out.println("\n1ª Semestre: Português\nNota: 8,5\n");
 				Thread.sleep(500);
-				System.out.printf("2ª Semestre: Português\nNota: 3\n");
-				Thread.sleep(500);
-				System.out.printf("\nVamos lá!");
+				System.out.println("\nVamos lá!");
 				Thread.sleep(1000);				
 				
-				System.out.printf("\n1ª Semestre: "); materia1 = ler.nextLine();
-				System.out.printf("Nota: "); nota1 = ler.nextInt();
+				ler.nextLine();
+				System.out.printf("\n1ª Semestre: ");
+				materia1 = ler.nextLine();
+				System.out.printf("Nota: ");
+				nota1 = ler.nextFloat();
 				
 				
-				System.out.printf("\n2ª Semestre: %s \n", materia1); //materia2 = ler.nextLine();
-				System.out.printf("Nota: "); nota2 = ler.nextInt();
+				System.out.printf("\n2ª Semestre: %s \n",materia1);
+				System.out.printf("Nota: ");
+				nota2 = ler.nextFloat();
 				
 				
-				System.out.printf("\n3ª Semestre: %s \n", materia1); //materia3 = ler.nextLine();
-				System.out.printf("Nota: "); nota3 = ler.nextInt();
+				System.out.printf("\n3ª Semestre: %s \n", materia1);
+				System.out.printf("Nota: ");
+				nota3 = ler.nextFloat();
 				
 				
-				System.out.printf("\n4ª Semestre: %s \n", materia1); //materia4 = ler.nextLine();
-				System.out.printf("Nota: "); nota4 = ler.nextInt();
+				System.out.printf("\n4ª Semestre: %s \n", materia1);
+				System.out.printf("Nota: ");
+				nota4 = ler.nextFloat();
 				
 				
-					System.out.printf("\nSua média é:\n %6.2f",((double)(nota1 + nota2 + nota3 + nota4) / 4));
+					System.out.printf("\nSua média é:\n %6.2f",((nota1 + nota2 + nota3 + nota4) / 4));
 					Thread.sleep(1000);
 					media = ((nota1 + nota2 + nota3 + nota4) / 4);
-					if (media >= 6){
-						System.out.println("\nParabéns pela sua nota!");
-					}
-					else System.out.println("\nEspero que você melhore suas notas na proxima vez!");
-					Thread.sleep(500);
-					System.out.printf("Até a próxima, %s!!!", nome);
 					
-						
+					
+					if (media >= mediapass){
+						System.out.println("\nParabéns, você esta aprovado(a)!");
+						Thread.sleep(500);
+					}
+					else if (media <= mediapass){
+					System.out.println("\nQue pena, você reprovou!\nEspero que você melhore suas notas, boa sorte!");
+					Thread.sleep(500);
+					}
+					System.out.printf("Até a próxima, %s!", nome);
+					
+					
 	}
 
 	
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "TarefaCalculoMedia" 
+"# TarefaCalculoMedia" 
